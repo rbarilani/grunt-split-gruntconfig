0f06fdf2cba3a30e5622228b7b082c2bf8b0f821
Ruben Barilani <ruben.barilani.dev@gmail.com>
2015-06-29 16:36:11 +0200

    updates .travis and package json scripts

---
+++ src/grunt-task.js
@@ -0,0 +1,17 @@
+var splitGruntconfig = require('../src/index.js');
+
+module.exports = function (grunt) {
+
+  grunt.registerTask(splitGruntconfig.TASK_NAME, splitGruntconfig.TASK_DESCRIPTION, function () {
+
+    var done    = this.async();
+
+    splitGruntconfig(grunt.config.getRaw(), this.options())
+      .then(function (){
+        grunt.log.ok('Files written!'); done();
+      })
+      .catch(function (error) {
+        grunt.log.error(error); done(false);
+      });
+  });
+};
